a1ab8df28997787dfef833d596135804d72f1cc0
Shaon22 <shaonroy11@gmail.com>
2023-12-25 16:35:01 +0600

    ready to ndeploy

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
-app.use(cors())
+app.use(cors({
+    origin:['https://jazzy-meringue-09a5cf.netlify.app','http://localhost:5173'],
+    credentials:true
+  }));
 app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sykxlbw.mongodb.net/?retryWrites=true&w=majority`;
@@ -24,12 +27,41 @@ async function run() {
     try {
         const taskCollection = client.db('task-management').collection('tasks')
         
+        app.get('/tasks/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await taskCollection.findOne(query)
+            res.send(result)
+        })
+      
+        app.patch('/tasks/:id',async(req,res)=>{
+        const id=req.params.id
+        const filter={_id: new ObjectId(id)}
+        const updatedInfo = req.body
+        const updatedDoc={
+            $set:{
+                title:updatedInfo.title,
+                description:updatedInfo.description,
+                deadline:updatedInfo.deadline,
+                priority:updatedInfo.priority,
+                status:updatedInfo.status
+            }
+        }
+        const result=await taskCollection.updateOne(filter,updatedDoc)
+        res.send(result)
+       })
+        app.delete('/tasks/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await taskCollection.deleteOne(query)
+            res.send(result)
+        })
         app.get('/tasks', async (req, res) => {
             const email = req.query.email
             const query = { email: email }
             const result = await taskCollection.find(query).toArray()
             res.send(result)
-          })
+        })
 
         app.post('/tasks', async (req, res) => {
             const taskInfo = req.body
@@ -43,7 +75,7 @@ async function run() {
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-       
+
     }
 }
 run().catch(console.dir);
